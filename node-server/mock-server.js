ed1050f9c8601ebd4595b871bbd5b5e56d514bf6
Hubert Muller <hubert@sportport.eu>
2021-06-24 19:38:24 +0200

    node serwer

--- node-server/mock-server.js
+++ node-server/mock-server.js
@@ -5,7 +5,26 @@ const server = http.createServer( (req, res) => {
     if (req.method === 'GET' && req.url === '/api/lista') {
         res.statusCode = 200;
         res.setHeader('Content-type', 'text/plain');
-        res.end('Czesc');
+        res.end(`[
+  {
+    "nazwa": "Moderna",
+    "kraj": "USA",
+    "cena": 20,
+    "waluta": "USD"
+  },
+  {
+    "nazwa": "Pfizer",
+    "kraj": "USA",
+    "cena": 10,
+    "waluta": "USD"
+  },
+  {
+    "nazwa": "Novavax",
+    "kraj": "USA",
+    "cena": 15,
+    "waluta": "USD"
+  }
+]`);
     } else {
         res.statusCode = 404;
         res.setHeader('Content-type', 'text/plain');
